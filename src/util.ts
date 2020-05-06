e819cdab656615057fc434bcdcdd7a892c5666f5
Lithl <blanketyblankman@gmail.com>
2020-05-06 08:10:59 -0700

    add unique filter function

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,7 @@
 export function assertUnreachable(_: never): never {
   throw new Error('This function should not be called');
 }
+
+export function onlyUnique<T>(v: T, i: number, self: T[]) {
+  return self.indexOf(v) === i;
+}
