bc78a0cbb145d9d5e375adc91fdf97bcd7b6f4ca
Lithl <blanketyblankman@gmail.com>
2020-05-15 02:04:36 -0700

    utility function array intersection

--- src/util.ts
+++ src/util.ts
@@ -5,3 +5,7 @@ export function assertUnreachable(_: never): never {
 export function onlyUnique<T>(v: T, i: number, self: T[]) {
   return self.indexOf(v) === i;
 }
+
+export function intersection<T>(a1: T[], a2: T[]) {
+  return a1.filter((v) => a2.indexOf(v) !== -1);
+}
