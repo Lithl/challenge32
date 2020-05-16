0ed60fc41d239cde7da6c9a77db00e151a92e057
Lithl <blanketyblankman@gmail.com>
2020-05-16 03:07:55 -0700

    created iconset-mtg

---
+++ src/components/iconset-mtg/index.ts
@@ -0,0 +1,15 @@
+import { PolymerElement, html } from '@polymer/polymer/polymer-element';
+import { customElement } from '@polymer/decorators';
+import '@polymer/iron-iconset-svg/iron-iconset-svg';
+
+import { default as template } from './template.html';
+
+import './index.scss?name=iconset-mtg';
+
+@customElement('iconset-mtg')
+export class IconsetMtg extends PolymerElement {
+  static get template() {
+    // @ts-ignore
+    return html([template]);
+  }
+}
